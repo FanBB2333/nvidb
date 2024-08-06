c714417a60f9a3b7a84c983f82d6fe71a6fc8978
L1ght <2504082957@qq.com>
2024-08-06 20:59:58 +0800

    [chore] update `setup.py` config

--- setup.py
+++ setup.py
@@ -1,9 +1,13 @@
 from setuptools import setup, find_packages
-
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 setup(
     name='nvidb',
     version='0.1',
     packages=find_packages(),
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     install_requires=[
         'pandas',
         'paramiko',
