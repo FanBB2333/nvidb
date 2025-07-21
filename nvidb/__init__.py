07908336ec74021fd4142e3d021659f668f43eba
FanBB2333 <2504082957@qq.com>
2025-07-21 12:07:48 +0800

    refactor: fix import path

--- nvidb/__init__.py
+++ nvidb/__init__.py
@@ -1,2 +1,8 @@
-from . import src as nvidb
+# Import main modules
+from . import utils
+from . import connection
+from . import data_modules
 from . import test
+
+# For backward compatibility
+from . import test as nvidb_test
