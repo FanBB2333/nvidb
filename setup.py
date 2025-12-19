72d6a3177daf7b4d56b45866c4b7998d3c9edd3e
FanBB2333 <2504082957@qq.com>
2025-12-19 22:39:17 +0800

    chore: bump version to v1.2.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ this_directory = Path(__file__).parent
 long_description = (this_directory / "README.md").read_text()
 setup(
     name='nvidb',
-    version='1.1.2',
+    version='1.2.0',
     packages=find_packages(),
     url="https://github.com/FanBB2333/nvidb/",
     long_description=long_description,
