3a1a7307add75d98f69e090b9e7c083a12244fb9
FanBB2333 <2504082957@qq.com>
2025-12-20 23:10:03 +0800

    chore: bump version to v1.4.3

--- nvidb/config.py
+++ nvidb/config.py
@@ -6,7 +6,7 @@ import os
 from pathlib import Path
 
 # Package version
-VERSION = "1.4.2"
+VERSION = "1.4.3"
 
 # Default working directory for nvidb data (config, logs, database)
 # Can be overridden by NVIDB_HOME environment variable
