722c15600140c7830bdb63e0ecc9ae839b5d0851
L1ght <2504082957@qq.com>
2026-09-14 17:09:12 +0800

    chore(release): 1.9.1

--- nvidb/config.py
+++ nvidb/config.py
@@ -7,7 +7,7 @@ import os
 from pathlib import Path
 
 # Package version
-VERSION = "1.9.0"
+VERSION = "1.9.1"
 
 # Default working directory for nvidb data (config, logs, database)
 # Can be overridden by NVIDB_HOME environment variable
